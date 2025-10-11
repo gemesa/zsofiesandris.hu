e71721f94e45a9d6d3cab16a0dd1e6aeb75e3516
Andras Gemes <gemesa@protonmail.com>
2025-10-11 18:59:18 +0200

    refactor: replace deprecated method

--- app/_lib/types.ts
+++ app/_lib/types.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const weddingApplicationSchema = z.object({
   fullName: z.string().nonempty("Kérjük, add meg a teljes neved!"),
-  email: z.string().email("Kérjük, add meg az e-mail címed!"),
+  email: z.email("Kérjük, add meg az e-mail címed!"),
   phoneNumber: z.string().nonempty("Kérjük, add meg a telefonszámod!"),
   otherGuests: z.string().optional(),
   foodRestrictions: z.string().optional(),
