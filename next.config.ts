5cf2a12e1a21d57db467c33982ddd7dfe53bebbe
Andras Gemes <gemesa@protonmail.com>
2025-12-08 18:52:20 +0100

    chore: remove depr. eslint config from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     qualities: [75, 100],
   },
